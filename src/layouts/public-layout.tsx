ba806172428a50000b3eb5ed1b90fd32482f6fca
Tushar Nautiyal <tusharnautiyal24@gmail.com>
2025-04-17 18:43:14 +0530

    Changes : Firebase Configurations & Userdata Saved Successfully using Handlers

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import AuthHandler from "@/handlers/auth-handler";
 import { Outlet } from "react-router-dom";
 
 
@@ -7,6 +8,7 @@ const PublicLayout = () => {
   return (
     <div className="w-full">
         {/* handler to store the user data -> if user not exist store data in firebase  */}
+        <AuthHandler />
         <Header />
 
         <Outlet />
