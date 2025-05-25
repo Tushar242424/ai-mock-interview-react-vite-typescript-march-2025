ac6d3469c08da280b53bf0b5f18eb857be448d79
Tushar Nautiyal <tusharnautiyal24@gmail.com>
2025-05-25 15:30:02 +0530

    Changes : Project Completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ import  MainLayout from "@/layouts/main-layout";
 import HomePage  from "@/routes/home";
 import SignInPage from "@/routes/sign-in";
 import SignUpPage from "@/routes/sign-up";
-
+import { Generate } from "./components/generate";
+import Dashboard from "./routes/dashboard";
+import CreateEditPage from "./routes/create-edit-page";
+import MockLoadPage from "./routes/mock-load-page";
+import MockInterviewPage from "./routes/mock-interview-page";
+import Feedback from "./routes/feedback";
 
 
 const App = () => {
@@ -28,9 +33,17 @@ const App = () => {
 
         {/* Protected Routes */}
         <Route element={<ProtectRoutes><MainLayout /></ProtectRoutes>}>
-{/* add all the protected routes  */}
-
-
+          {/* add all the protected routes  */}
+          <Route element={<Generate />} path="/generate">
+            <Route index element={<Dashboard />}/>
+            <Route path=":interviewId" element={<CreateEditPage />} />
+            <Route path="interview/:interviewId" element={<MockLoadPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from '@clerk/clerk-react'
 
 import './index.css'
 import App from './App.tsx'
+import { ToasterProvider } from './provider/toast-provider.tsx'
+
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -18,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
+      <ToasterProvider />
     </ClerkProvider>
   </StrictMode>,
 );
